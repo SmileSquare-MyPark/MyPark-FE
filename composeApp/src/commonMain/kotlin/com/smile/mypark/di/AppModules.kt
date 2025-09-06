212be727edec043f5fc57b51fc5a13b7a3407c7a
hangunhee39 <ghee0831@naver.com>
2025-09-06 15:05:40 +0900

    fix/ ios 빌드

--- composeApp/src/commonMain/kotlin/com/smile/mypark/di/AppModules.kt
+++ composeApp/src/commonMain/kotlin/com/smile/mypark/di/AppModules.kt
@@ -1,7 +1,6 @@
 package com.smile.mypark.di
 
 import org.koin.core.context.startKoin
-import org.koin.core.module.Module
 import org.koin.dsl.module
 
 
--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -7,6 +7,7 @@ struct ComposeView: UIViewControllerRepresentable {
         MainViewControllerKt.MainViewController()
     }
 
+
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
 
