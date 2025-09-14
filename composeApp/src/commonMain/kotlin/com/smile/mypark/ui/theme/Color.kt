6656c803049e6e89b405bf4ed828a2d8c92df9fe
Eouls <ckrlcks00@gmail.com>
2025-09-15 03:13:47 +0900

    add/이미지, 컬러추가

--- composeApp/src/commonMain/kotlin/com/smile/mypark/ui/theme/Color.kt
+++ composeApp/src/commonMain/kotlin/com/smile/mypark/ui/theme/Color.kt
@@ -1,5 +1,41 @@
 package com.smile.mypark.ui.theme
 
-fun Color(){
+import androidx.compose.ui.graphics.Color
 
-}
+val Primary = Color(0xFF04925E)
+val Sub = Color(0xFF32C570)
+val PrimaryLight = Color(0xFFD3F2E0)
+
+val CharcoalGray    = Color(0xFF333333)
+val DarkGray77      = Color(0xFF4D4D4D)
+val DarkGray93      = Color(0xFF5D5D5D)
+val DarkGray102     = Color(0xFF666666)
+val DarkGray        = Color(0xFF737373)
+val NeutralGray     = Color(0xFF808080)
+val Gray134         = Color(0xFF868686)
+val MediumGray      = Color(0xFF8B8B8B)
+val Gray153         = Color(0xFF999999)
+val Gray160         = Color(0xFFA0A0A0)
+val LightGray170    = Color(0xFFAAAAAA)
+val MediumLightGray = Color(0xFFACACAC)
+val LightGray179    = Color(0xFFB3B3B3)
+val LightGray217    = Color(0xFFD9D9D9)
+val VeryLightGray230 = Color(0xFFE6E6E6)
+val VeryLightGray242 = Color(0xFFF2F2F2)
+
+val CoralRed        = Color(0xFFFC6A6B)
+val Amber           = Color(0xFFE99800)
+
+val LeafGreen       = Color(0xFF417F2C)
+val LimeGreen       = Color(0xFF9FC532)
+
+val MintWhiteE9     = Color(0xFFE9F3F2)
+val MintWhiteF2     = Color(0xFFF2FFF6)
+
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val DarkBlue = Color(0xFF000087)
+val Red = Color(0xFFE82424)
+val Green = Color(0xFF3CCF60)
+val Pink = Color(0xFFFCA3B9)
+val Gray = Color(0xFF8EA3A6)
